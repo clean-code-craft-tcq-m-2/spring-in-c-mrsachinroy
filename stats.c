f0b851e54ba46a323e16ae5f7cb61b1f2757ad11
SACHIN ROY <sachinkumarsnp@gmail.com>
2022-10-21 17:16:30 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,16 +1,33 @@
 #include "stats.h"
 #include "stdlib.h"
-// #include "studio.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
+    float sum;
+    // int NoOfElement = 
     s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    s.min = 0xFFFF;
+    s.max = -99999;
 
+    for (int i = 0; i < setlength; i++)
+    {
+        sum += *numberset;
+
+        if (*numberset > s.max)
+        {
+            s.max = *numberset;
+        }
+
+        if (*numberset < s.min)
+        {
+            s.min = *numberset;
+        }
+
+        numberset++;
+    }
+    s.average = sum/setlength;
     return s;
 
-    // printf("Hello There!!!");
 }
 
 int emailAlertCallCount = 0;
