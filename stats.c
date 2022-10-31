b562cef533cb94078579c731793a6d0c59da1eba
SACHIN ROY <sachinkumarsnp@gmail.com>
2022-10-31 12:53:29 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -58,8 +58,8 @@ void ledAlerter()
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
     int size = 2; //sizeof(alerters);/// sizeof(alerters[0]);
-    (alerters[0])();
-    (alerters[1])();
+    // (alerters[0])();
+    // (alerters[1])();
     if (maxThreshold > computedStats.max )
     {
         for (int i = 0; i < size; i++)
